5be97dc26a814532739c7ad4da1f0cd32a80b5bc
AniketChavan-debug <aniketchavan971412@gmail.com>
2025-04-05 01:26:54 +0530

    Create authRoutes.js

---
+++ authRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+// Register
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+
+    try {
+        await user.save();
+        res.status(201).send('User registered successfully!');
+    } catch (err) {
+        res.status(400).send('Error registering user.');
+    }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } else {
+        res.status(400).send('Invalid credentials.');
+    }
+});
+
+module.exports = router;
